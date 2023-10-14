bed2638d7383166d0e1f89642207c515bec96e40
Emmanuel <oyedelee22@gmail.com>
2023-10-14 07:44:23 +0100

    update

--- src/components/Our_drive.vue
+++ src/components/Our_drive.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="w-[90%] mx-auto xl:w-[80%]">
         <div class="py-[7.4rem]">
-            <h2 data-aos="fade-out" data-aos-duration="300s" data-easing="ease-in" class="mb-[1.7rem] md:text-[2rem] text-[5vw] font-semibold text-[#000066] font-[Poppins]">OUR DRIVE</h2>
+            <h2 data-aos="fade" data-aos-duration="300s" data-aos-easing="ease-in" class="mb-[1.7rem] md:text-[2rem] text-[5vw] font-semibold text-[#000066] font-[Poppins]">OUR DRIVE</h2>
 
             <div class="flex flex-col md:flex-row md:justify-center lg:flex-nowrap md:flex-wrap px-10 gap-10">
                 <div v-for="ourDrive in driveData" :key="ourDrive.id"  class="text-center md:w-[45%] grid place-items-center md:block first-letter:lg:w-[33.3%] ">
                         <Card
-                        data-aos="fade-in" data-aos-duration="300s" data-easing="ease-out"
+                        data-aos="fade" 
+                        data-aos-duration="300s" 
+                        data-aos-easing="ease-out"
                         :icon="ourDrive.icon"
                         :title="ourDrive.title"
                         :text="ourDrive.text" />
